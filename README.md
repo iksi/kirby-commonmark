f18cc8570faa130e48431a138501be50f3137329
iksi <jurriaan@iksi.cc>
2017-11-03 12:17:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Kirby CommonMark
+
+Replaces Kirby’s markdown component for one that uses [CommonMark](http://commonmark.org/). It uses [league/commonmark](http://commonmark.thephpleague.com/), a Markdown parser for PHP which supports the full [CommonMark spec](http://spec.commonmark.org/).
+
+## Installation
+
+`git submodule add git@github.com:iksi/kirby-commonmark.git site/plugins/kirby-commonmark`
+
+Or put the contents of the repository in `site/plugins/kirby-commonmark`.
+
+## License
+
+[MIT](LICENSE.md)
---
+++ kirby-commonmark.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Kirby\Plugin;
+
+require_once __DIR__ . DS . 'vendor' . DS . 'autoload.php';
+
+use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Environment;
+
+class KirbyCommonMark extends \Kirby\Component\Markdown {
+
+  public function defaults() {
+    return [
+      'plugin.commonmark' => true,
+      'plugin.commonmark.environment' => Environment::createCommonMarkEnvironment(),
+      'plugin.commonmark.breaks' => true
+    ];
+  }
+
+  public function parse($commonmark) {
+    if(!$this->kirby->options['plugin.commonmark']) {
+      return $commonmark;
+    } else {
+      $converter = new CommonMarkConverter([
+        'renderer' => [
+          'soft_break' => $this->kirby->options['plugin.commonmark.breaks'] ? '<br>' : PHP_EOL
+        ]
+      ], $this->kirby->options['plugin.commonmark.environment']);
+
+      return $converter->convertToHtml($commonmark);
+    }
+  }
+
+}
+
+kirby()->set('component', 'markdown', 'Kirby\Plugin\KirbyCommonMark');
